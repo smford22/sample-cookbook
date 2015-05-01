225fd44803c08235eb62ca78042a190f8d67b349
Scott Ford <smford22@gmail.com>
2015-05-01 12:06:29 -0500

    updating .kitchen.yml to support centos-66 and updating serverspec

--- test/integration/default/serverspec/localhost/server_spec.rb
+++ test/integration/default/serverspec/localhost/server_spec.rb
@@ -4,4 +4,8 @@ describe 'server' do
   it 'has apache installed' do
     expect(package 'httpd').to be_installed
   end
+ 
+  it 'http is listening' do
+    expect(service 'httpd').to be_running
+  end
 end
